6363fa1ffb08778da07030a0c5367f887dc49758
vishal7100jain <220101005026@gandhinagaruni.ac.in>
2025-07-06 21:24:54 +0530

    fix : fixed the delete api

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -1,49 +1,130 @@
-import React, { useState, useEffect } from 'react';
-import { TextField, Button, Typography, Paper } from '@material-ui/core';
+/* eslint-disable react/react-in-jsx-scope */
+import { Button, Paper, TextField, Typography } from '@material-ui/core';
+import { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import FileBase from 'react-file-base64';
 
-import useStyles from './styles';
 import { createPost, updatePost } from '../../actions/posts';
+import useStyles from './styles';
 
 const Form = ({ currentId, setCurrentId }) => {
-  const [postData, setPostData] = useState({ creator: '', title: '', message: '', tags: '', selectedFile: '' });
+  const [postData, setPostData] = useState({
+    creator: '',
+    title: '',
+    message: '',
+    tags: '',
+  });
+  const [selectedFile, setSelectedFile] = useState(null);
   const post = useSelector((state) => (currentId ? state.posts.find((message) => message._id === currentId) : null));
   const dispatch = useDispatch();
   const classes = useStyles();
 
   useEffect(() => {
-    if (post) setPostData(post);
+    if (post) {
+      setPostData({
+        creator: post.creator,
+        title: post.title,
+        message: post.message,
+        tags: post.tags.join(','), // convert array to string
+      });
+    }
   }, [post]);
 
   const clear = () => {
     setCurrentId(0);
-    setPostData({ creator: '', title: '', message: '', tags: '', selectedFile: '' });
+    setPostData({ creator: '', title: '', message: '', tags: '' });
+    setSelectedFile(null);
   };
 
-  const handleSubmit = async (e) => {
+  const handleSubmit = (e) => {
     e.preventDefault();
 
-    if (currentId === 0) {
-      dispatch(createPost(postData));
-      clear();
-    } else {
-      dispatch(updatePost(currentId, postData));
-      clear();
+    const formData = new FormData();
+    formData.append('creator', postData.creator);
+    formData.append('title', postData.title);
+    formData.append('message', postData.message);
+    formData.append('tags', postData.tags);
+    if (selectedFile) {
+      formData.append('selectedFile', selectedFile);
     }
+
+    if (currentId === 0) {
+      dispatch(createPost(formData));
+    } else {
+      dispatch(updatePost(currentId, formData));
+    }
+    clear();
   };
 
   return (
     <Paper className={classes.paper}>
-      <form autoComplete="off" noValidate className={`${classes.root} ${classes.form}`} onSubmit={handleSubmit}>
-        <Typography variant="h6">{currentId ? `Editing "${post.title}"` : 'Creating a Memory'}</Typography>
-        <TextField name="creator" variant="outlined" label="Creator" fullWidth value={postData.creator} onChange={(e) => setPostData({ ...postData, creator: e.target.value })} />
-        <TextField name="title" variant="outlined" label="Title" fullWidth value={postData.title} onChange={(e) => setPostData({ ...postData, title: e.target.value })} />
-        <TextField name="message" variant="outlined" label="Message" fullWidth multiline rows={4} value={postData.message} onChange={(e) => setPostData({ ...postData, message: e.target.value })} />
-        <TextField name="tags" variant="outlined" label="Tags (coma separated)" fullWidth value={postData.tags} onChange={(e) => setPostData({ ...postData, tags: e.target.value.split(',') })} />
-        <div className={classes.fileInput}><FileBase type="file" multiple={false} onDone={({ base64 }) => setPostData({ ...postData, selectedFile: base64 })} /></div>
-        <Button className={classes.buttonSubmit} variant="contained" color="primary" size="large" type="submit" fullWidth>Submit</Button>
-        <Button variant="contained" color="secondary" size="small" onClick={clear} fullWidth>Clear</Button>
+      <form
+        autoComplete="off"
+        noValidate
+        className={`${classes.root} ${classes.form}`}
+        onSubmit={handleSubmit}
+      >
+        <Typography variant="h6">
+          {currentId ? `Editing "${post?.title}"` : 'Creating a Memory'}
+        </Typography>
+        <TextField
+          name="creator"
+          variant="outlined"
+          label="Creator"
+          fullWidth
+          value={postData.creator}
+          onChange={(e) => setPostData({ ...postData, creator: e.target.value })}
+        />
+        <TextField
+          name="title"
+          variant="outlined"
+          label="Title"
+          fullWidth
+          value={postData.title}
+          onChange={(e) => setPostData({ ...postData, title: e.target.value })}
+        />
+        <TextField
+          name="message"
+          variant="outlined"
+          label="Message"
+          fullWidth
+          multiline
+          rows={4}
+          value={postData.message}
+          onChange={(e) => setPostData({ ...postData, message: e.target.value })}
+        />
+        <TextField
+          name="tags"
+          variant="outlined"
+          label="Tags (comma separated)"
+          fullWidth
+          value={postData.tags}
+          onChange={(e) => setPostData({ ...postData, tags: e.target.value })}
+        />
+        <div className={classes.fileInput}>
+          <input
+            type="file"
+            onChange={(e) => setSelectedFile(e.target.files[0])}
+          />
+        </div>
+        <Button
+          className={classes.buttonSubmit}
+          variant="contained"
+          color="primary"
+          size="large"
+          type="submit"
+          fullWidth
+        >
+          Submit
+        </Button>
+        <Button
+          variant="contained"
+          color="secondary"
+          size="small"
+          onClick={clear}
+          fullWidth
+        >
+          Clear
+        </Button>
       </form>
     </Paper>
   );
--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -73,7 +73,7 @@ export const deletePost = async (req, res) => {
   if (!mongoose.Types.ObjectId.isValid(id))
     return res.status(404).send(`No post with id: ${id}`);
 
-  await PostMessage.findByIdAndRemove(id);
+  await PostMessage.findOneAndDelete({ _id: id });
 
   res.json({ message: "Post deleted successfully." });
 };
